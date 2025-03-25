7939bc9388f8f6b591322bfe7551987d30588f20
phuoctv <phuoctv.tgg@vnpt.vn>
2025-03-25 22:21:40 +0700

    first commit

---
+++ src/main/java/com/example/testk8s/Testk8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.testk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testk8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testk8s/Testk8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testk8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
